f2c4094098090f1d72151588410ee692f99df111
fulder <misad90@gmail.com>
2020-02-20 20:52:18 +0100

    Add width and depth rows

--- include/slider_val.js
+++ include/slider_val.js
@@ -1,6 +1,12 @@
 const heightSlider = document.getElementById("height_slider");
 const heightField = document.getElementById("height_field");
 
+const widthSlider = document.getElementById("width_slider");
+const widthField = document.getElementById("width_field");
+
+const depthSlider = document.getElementById("depth_slider");
+const depthField = document.getElementById("depth_field");
+
 heightSlider.oninput = function() {
     heightField.value = this.value;
 }
@@ -8,5 +14,25 @@ heightField.oninput = function() {
     heightSlider.value = this.value;
     if (this.value === "") {
         heightSlider.value = 0;
+    }
+}
+
+widthSlider.oninput = function() {
+    widthField.value = this.value;
+}
+widthField.oninput = function() {
+    widthSlider.value = this.value;
+    if (this.value === "") {
+        widthSlider.value = 0;
+    }
+}
+
+depthSlider.oninput = function() {
+    depthField.value = this.value;
+}
+depthField.oninput = function() {
+    depthSlider.value = this.value;
+    if (this.value === "") {
+        depthSlider.value = 0;
     }
 }
