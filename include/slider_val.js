2313a1105d47ba53c775d4319d3ee2f9fe3551f4
fulder <misad90@gmail.com>
2020-02-20 20:29:49 +0100

    Show slider value in field

--- include/slider_val.js
+++ include/slider_val.js
@@ -2,5 +2,5 @@ const heightSlider = document.getElementById("height_slider");
 const heightFIeld = document.getElementById("height_field");
 
 heightSlider.oninput = function() {
-    console.log(this.value);
+    heightFIeld.value = this.value;
 }
