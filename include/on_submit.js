a7cff7340392531f6769a0c6a2c9c24ac4ef7953
fulder <misad90@gmail.com>
2020-02-20 21:33:11 +0100

    Check normal letter

--- include/on_submit.js
+++ include/on_submit.js
@@ -1,3 +1,19 @@
 function calculateType() {
+    const height = document.getElementById("height_field").value;
+    const width = document.getElementById("width_field").value;
+    const depth = document.getElementById("depth_field").value;
 
+    if normalLetter(height, width, depth) {
+        console.log("NORMAL LETTER")
+    }
+}
+
+function normalLetter(height, width, depth) {
+    if (height > 60 || width > 60 || depth > 60) {
+        return false;
+    }
+    if (height + width + depth > 90) {
+        return false;
+    }
+    return true;
 }
